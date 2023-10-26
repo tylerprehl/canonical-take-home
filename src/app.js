1c3b5006427dc5c18341a608e17088d5d4d94f0f
Tyler Prehl <tyler@prehl.us>
2023-10-25 21:49:02 -0400

    Get Title, Author, and Date from API call
    Update app.js to create an async function that gets the json response
    from the provided API URL, and create a for loop that logs the title,
    author, and date (correctly formatted) for each item in the response
    json.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
 import './style.scss';
+
+
+async function getApiData(apiUrl) {
+    const response = await fetch (apiUrl, {mode: 'cors'});
+    const responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+}
+
+let apiData = await getApiData('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json');
+
+apiData.forEach(async (articleInfo) => {
+    const monthOptions = {month: 'long'}
+    const monthFormatter = new Intl.DateTimeFormat('en-US', monthOptions);
+
+    const creationDate = new Date(articleInfo.date);
+    const day = creationDate.getDate();
+    const month = monthFormatter.format(creationDate);
+    const year = creationDate.getFullYear();
+    const dateFormatted = day + " " + month + " " + year;
+    
+
+    console.log(`
+    Title: ${articleInfo.title.rendered} \n
+    Date: ${dateFormatted} \n
+    Author: ${articleInfo._embedded.author[0].name}
+    `)
+});
