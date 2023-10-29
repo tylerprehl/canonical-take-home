584a41bb6eb4deed5819ebe2e55d78e8c826fb50
Tyler Prehl <tyler@prehl.us>
2023-10-29 14:35:43 -0400

    Extract necessary API information & create new cards
    Implement JavaScript to extract the rest of the required information
    for each Article card (author link, image URL, image alternate text).
    Create function that takes a hidden template of the card and fills in
    the template with the API information for each article found.

--- src/app.js
+++ src/app.js
@@ -8,6 +8,25 @@ async function getApiData(apiUrl) {
     return responseJson;
 }
 
+function createArticleCard(topic, title, date, author, authorLink, imageUrl, imageAlt) {
+    console.log(`ImageURL: ${imageUrl}`);
+    const articleCardTemplate = document.querySelector('.p-card.template');
+    const newArticleCard = articleCardTemplate.cloneNode(true);
+    newArticleCard.hidden = false;
+
+    newArticleCard.querySelector('.topic').textContent = topic;
+    newArticleCard.querySelector('.title').textContent = title;
+    newArticleCard.querySelector('.date').textContent = date;
+    const authorElement = newArticleCard.querySelector('.author');
+    authorElement.textContent = author;
+    authorElement.href = authorLink;
+    newArticleCard.querySelector('.p-card__image').src = imageUrl;
+    newArticleCard.querySelector('.p-card__image').alt = imageAlt;
+
+    const articleCardsContainer = document.querySelector('div.col-5');
+    articleCardsContainer.appendChild(newArticleCard);
+}
+
 let apiData = await getApiData('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json');
 
 apiData.forEach(async (articleInfo) => {
@@ -19,11 +38,44 @@ apiData.forEach(async (articleInfo) => {
     const month = monthFormatter.format(creationDate);
     const year = creationDate.getFullYear();
     const dateFormatted = day + " " + month + " " + year;
-    
+
+    let imageUrl = '';
+    let imageAlt = '';
+    if (JSON.stringify(articleInfo._embedded).includes('wp:featuredmedia')) {
+        imageUrl = JSON.stringify(articleInfo._embedded['wp:featuredmedia'][0].source_url).replace('admin.','');
+        imageAlt = articleInfo._embedded['wp:featuredmedia'][0].title.rendered;
+    }
+    else {
+        imageUrl = 'default URL';
+        imageAlt = 'default Image Alt'
+    }
+
+    let topic = '';
+    if (JSON.stringify(articleInfo._embedded['wp:term'][2]).includes('name')) { 
+        topic = articleInfo._embedded['wp:term'][2][0].name; 
+    }
+    else { 
+        topic = 'No topic';
+    }
+
+    const title = articleInfo.title.rendered;
+    const author = articleInfo._embedded.author[0].name;
+    const authorLink = articleInfo._embedded.author[0].link;
 
     console.log(`
-    Title: ${articleInfo.title.rendered} \n
+    Topic: ${topic} \n
+    Title: ${title} \n
     Date: ${dateFormatted} \n
-    Author: ${articleInfo._embedded.author[0].name}
+    Author: ${author} \n
+    Author Link: ${authorLink} \n
+    Image URL: ${imageUrl}
+    Image Alt: ${imageAlt}
     `)
+
+    createArticleCard(topic, title, dateFormatted, author, authorLink, imageUrl, imageAlt);
 });
+
+/*
+Canonical Default Image Reference:
+URL: https://www.hackster.io/news/canonical-launches-ubuntu-pro-with-10-years-updates-offers-individuals-five-free-machine-licenses-25348242f20b
+*/
