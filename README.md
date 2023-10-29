2654523be8d83f23a59a410fdfff38a546987aaf
Tyler Prehl <tyler@prehl.us>
2023-10-29 17:51:18 -0400

    Resolve image loading issue
    Also update README with Takeaways and Dependencies

--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # Canonical Take Home Test
-This repository houses my code for the Canonical Take Home Test part of the application process. The webpage produced can either be viewed (the easy way) by using the GitHub pages URL (https://tylerprehl.github.io/canonical-take-home/) or by following the steps below to serve the project locally.
+This repository houses my code for the Canonical Take Home Test part of the application process. The webpage produced can either be viewed (the easy way) by using the GitHub pages URL (https://tylerprehl.github.io/canonical-take-home/) or (the hard way) by following the "To Build Project" steps below to serve the project locally.
 
-### Steps to Serve the Webpage on LocalHost:8080
-1) Make a clone of the repo in your designated directory
-2) TBD
+### Site Functionality
+1) Uses and API call to get data about various articles
+2) Loads information about those articles into "cards"
+3) Displays the cards for the user to view/select as their leisure
+
+### To Build Project
+1) Install dependencies as necessary
+2) ...
 
 ### Takeaways
 <ul>
 <li>Setting up the Vanilla Framework is not as easy as following the <a href="https://vanillaframework.io/docs/building-vanilla">Building with Vanilla instructions</a></li>
-<li>Using a framework like Vanilla is pretty astounding for quick design production. Once I got Webpack, JavaScript, and SASS finally started working together properly, it was astoundingly easy to use the various pieces of the framework (such as the card pattern).
+<li>Using a framework like Vanilla is pretty astounding for quick design production. Once I got Webpack, JavaScript, and SASS finally started working together properly, it was astoundingly easy to use the various pieces of the framework (such as the card pattern). With that said, it starts to become difficult to make look nice and even when things are not standardized (such as image sizes).
+<li>JSON.Stringefy encloses the string in quotation marks, so trying to use said string as an absolute src path results in JavaScript thinking it's a relative path
 </ul>
 
+### Dependencies
+<ul>
+<li>css-loader - ^6.8.1
+<li>node-sass - ^9.0.0
+<li>sass - ^1.69.4
+<li>sass-loader - ^13.3.2
+<li>style-loader - ^3.3.3
+<li>vanilla-framework - ^4.5.0
+<li>webpack - ^5.89.0
+<li>webpack-cli - ^5.1.4
+<li>webpack-dev-server - ^4.15.1
+</ul>
--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import './style.scss';
 
 
-async function getApiData(apiUrl) {
-    const response = await fetch (apiUrl, {mode: 'cors'});
-    const responseJson = await response.json();
-    console.log(responseJson);
-    return responseJson;
+
+
+function formatDate(dateProvided) {
+    const monthOptions = {month: 'long'}
+    const monthFormatter = new Intl.DateTimeFormat('en-US', monthOptions);
+
+    const day = dateProvided.getDate();
+    const month = monthFormatter.format(dateProvided);
+    const year = dateProvided.getFullYear();
+    return day + " " + month + " " + year;
 }
 
 function createArticleCard(topic, title, date, author, authorLink, imageUrl, imageAlt) {
@@ -23,44 +28,59 @@ function createArticleCard(topic, title, date, author, authorLink, imageUrl, ima
     newArticleCard.querySelector('.p-card__image').src = imageUrl;
     newArticleCard.querySelector('.p-card__image').alt = imageAlt;
 
-    const articleCardsContainer = document.querySelector('div.col-5');
+    const articleCardsContainer = document.querySelector('div.row');
     articleCardsContainer.appendChild(newArticleCard);
 }
 
-let apiData = await getApiData('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json');
-
-apiData.forEach(async (articleInfo) => {
-    const monthOptions = {month: 'long'}
-    const monthFormatter = new Intl.DateTimeFormat('en-US', monthOptions);
-
-    const creationDate = new Date(articleInfo.date);
-    const day = creationDate.getDate();
-    const month = monthFormatter.format(creationDate);
-    const year = creationDate.getFullYear();
-    const dateFormatted = day + " " + month + " " + year;
-
+function getImageInformation(articleInfo) {
     let imageUrl = '';
     let imageAlt = '';
     if (JSON.stringify(articleInfo._embedded).includes('wp:featuredmedia')) {
-        imageUrl = JSON.stringify(articleInfo._embedded['wp:featuredmedia'][0].source_url).replace('admin.','');
-        imageAlt = articleInfo._embedded['wp:featuredmedia'][0].title.rendered;
+        imageUrl = articleInfo._embedded['wp:featuredmedia'][0].source_url.replace('admin.','');
+        imageAlt = articleInfo._embedded['wp:featuredmedia'][0].title.rendered;   
     }
     else {
-        imageUrl = 'default URL';
-        imageAlt = 'default Image Alt'
+        imageUrl = '../images/canonical-default-image-1.webp';
+        imageAlt = 'Canonical Ubuntu';
     }
+    return [imageUrl, imageAlt];
+}
 
+function getTopic(articleInfo) {
     let topic = '';
     if (JSON.stringify(articleInfo._embedded['wp:term'][2]).includes('name')) { 
         topic = articleInfo._embedded['wp:term'][2][0].name; 
     }
     else { 
-        topic = 'No topic';
+        topic = 'Hot Off the Press';
     }
+    return topic;
+}
+/* 
+async function downloadImage(imageSrc) {
+    console.log(imageSrc);
+} */
 
+async function getApiData(apiUrl) {
+    const response = await fetch (apiUrl, {mode: 'cors'});
+    const responseJson = await response.json();
+    console.log(responseJson);
+    return responseJson;
+}
+
+let apiData = await getApiData('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json');
+
+apiData.forEach(async (articleInfo) => {
+    const dateFormatted = formatDate(new Date(articleInfo.date));
+
+    const topic = getTopic(articleInfo);
     const title = articleInfo.title.rendered;
     const author = articleInfo._embedded.author[0].name;
     const authorLink = articleInfo._embedded.author[0].link;
+
+    const imageInfo = getImageInformation(articleInfo);
+    const imageUrl = imageInfo[0];
+    const imageAlt = imageInfo[1];
 
     console.log(`
     Topic: ${topic} \n
@@ -68,7 +88,7 @@ apiData.forEach(async (articleInfo) => {
     Date: ${dateFormatted} \n
     Author: ${author} \n
     Author Link: ${authorLink} \n
-    Image URL: ${imageUrl}
+    Image URL: ${imageUrl} \n
     Image Alt: ${imageAlt}
     `)
 
@@ -76,6 +96,6 @@ apiData.forEach(async (articleInfo) => {
 });
 
 /*
-Canonical Default Image Reference:
+"Canonical Ubuntu" Default Image Reference:
 URL: https://www.hackster.io/news/canonical-launches-ubuntu-pro-with-10-years-updates-offers-individuals-five-free-machine-licenses-25348242f20b
 */
